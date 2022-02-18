8cf5d270696c099ef4c4fd979cffa91ca5aff92f
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2022-02-17 21:19:34 -0300

    add haarcascade

--- main.cpp
+++ main.cpp
@@ -4,42 +4,62 @@
 using namespace std;
 using namespace cv;
 
+void detectAndDisplay( Mat frame );
+
+CascadeClassifier face_cascade;
+
 int main(int argc, char* argv[]){
 
+  //argv[1]
   VideoCapture cap(argv[1], cv::CAP_ANY); 
   if(!cap.isOpened()){
     cout << "Error opening video stream or file" << endl;
     return -1;
   }
+  String face_cascade_name = samples::findFile("data/haarcascades/haarcascade_frontalface_default.xml");
 
-  while(cap.isOpened()){
-    Mat frame;
-    // Capture frame-by-frame
-    cap >> frame;
+  if( !face_cascade.load( face_cascade_name ) )
+  {
+      cout << "--(!)Error loading face cascade\n";
+      return -1;
+  };
 
-    // If the frame is empty, break immediately
+  Mat frame;
+  while(cap.read(frame)){
+
     if (frame.empty()){
       break;
     }
 
-    // Display the resulting frame
-    imshow( "Frame", frame );
+    detectAndDisplay(frame);
+    
 
-    // Press  ESC on keyboard to exit
     char c=(char)waitKey(25);
     if(c==27){
       break;
     }
   }
-
-  cout << "CAMERA CAIU" << endl;
  
-  // When everything done, release the video capture object
   cap.release();
 
-  // Closes all the frames
   destroyAllWindows();
   
   return 0;
 
+}
+
+void detectAndDisplay(Mat frame){
+  Mat frame_gray;
+  cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+  equalizeHist(frame_gray, frame_gray);
+  vector<Rect> faces;
+  face_cascade.detectMultiScale(frame_gray, faces);
+
+  for ( size_t i = 0; i < faces.size(); i++ ){
+    Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+    ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4 );
+    Mat faceROI = frame_gray( faces[i] );
+  }
+  resize(frame,frame, Size(1280,720));
+  imshow( "Frame", frame );
 }
